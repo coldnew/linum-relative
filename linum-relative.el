0561bb6ed38ec6959643e3bcebef4be8912e3570
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-10-15 12:24:58 +0800

    Fix indention
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- linum-relative.el
+++ linum-relative.el
@@ -117,37 +117,36 @@ linum-releative will show the real line number at current line."
 ;;;; Advices
 (defadvice linum-update (before relative-linum-update activate)
   "This advice get the last position of linum."
-  (if (and (boundp 'helm-alive-p) helm-alive-p)
+  (if (bound-and-true-p helm-alive-p)
       (setq linum-relative-last-pos (helm-candidate-number-at-point))
-      (setq linum-relative-last-pos (line-number-at-pos))))
+    (setq linum-relative-last-pos (line-number-at-pos))))
 
 ;;;; Functions
 (defun linum-relative (line-number)
-  (when (and (boundp 'helm-alive-p) helm-alive-p)
+  (when (bound-and-true-p helm-alive-p)
     (with-helm-buffer
       (if (looking-at helm-candidate-separator)
           (setq line-number (save-excursion
                               (forward-line 1) (helm-candidate-number-at-point)))
-          (setq line-number (helm-candidate-number-at-point)))))
+        (setq line-number (helm-candidate-number-at-point)))))
   (let* ((diff1 (abs (- line-number linum-relative-last-pos)))
-	 (diff (if (minusp diff1)
-		   diff1
-		 (+ diff1 linum-relative-plusp-offset)))
-	 (current-p (= diff linum-relative-plusp-offset))
-	 (current-symbol (if (and linum-relative-current-symbol current-p)
-			     (if (string= "" linum-relative-current-symbol)
-				 (number-to-string line-number)
-			       linum-relative-current-symbol)
-			   (number-to-string diff)))
-	 (face (if current-p 'linum-relative-current-face 'linum)))
-    (if (and (boundp 'helm-alive-p)
-             helm-alive-p
-             (with-helm-buffer
-               (or (looking-at helm-candidate-separator)
-                   (eq (point-at-bol) (point-at-eol))
-                   (helm-pos-header-line-p))))
-        (propertize (format linum-relative-format current-symbol) 'invisible t)
-        (propertize (format linum-relative-format current-symbol) 'face face))))
+         (diff (if (minusp diff1)
+                   diff1
+                 (+ diff1 linum-relative-plusp-offset)))
+         (current-p (= diff linum-relative-plusp-offset))
+         (current-symbol (if (and linum-relative-current-symbol current-p)
+                             (if (string= "" linum-relative-current-symbol)
+                                 (number-to-string line-number)
+                               linum-relative-current-symbol)
+                           (number-to-string diff)))
+         (face (if current-p 'linum-relative-current-face 'linum)))
+    (if  (bound-and-true-p helm-alive-p)
+        (with-helm-buffer
+          (or (looking-at helm-candidate-separator)
+              (eq (point-at-bol) (point-at-eol))
+              (helm-pos-header-line-p))))
+    (propertize (format linum-relative-format current-symbol) 'invisible t)
+    (propertize (format linum-relative-format current-symbol) 'face face))))
 
 (defun linum-relative-on ()
   "Turn ON linum-relative."
@@ -168,7 +167,7 @@ linum-releative will show the real line number at current line."
 
 ;;;###autoload
 (define-minor-mode linum-relative-mode
-    "Display relative line numbers for current buffer."
+  "Display relative line numbers for current buffer."
   :group 'linum-relative
   :global t
   :lighter " LR"
@@ -176,8 +175,8 @@ linum-releative will show the real line number at current line."
       (progn
         (linum-relative-on)
         (global-linum-mode 1))
-      (linum-relative-off)
-      (global-linum-mode -1)))
+    (linum-relative-off)
+    (global-linum-mode -1)))
 
 
 (provide 'linum-relative)
