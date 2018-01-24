9fd6c7a75d5d5d0f4e4ceb42c8b9d5c769d30372
Yen-Chin Lee <coldnew.tw@gmail.com>
2018-01-24 18:25:20 +0800

    Add display-line-numbers-mode as backend.
    linum-mode is really slow on large file, since emacs 26.1 shipped with
    `display-line-number-mode', we can add it to linum-relative's backend
    to make relative number display has a nice speed.
    
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- README.md
+++ README.md
@@ -29,13 +29,27 @@ The non-interactive function *linum-on* (which should already be built into rece
 
     (linum-on)
 
-and alternatively, by enabling `linum-mode`:
+and alternatively, by using command:
 
-    M-x linum-mode
+    M-x linum-relative-mode
 
 Relative line numbering should already be enabled by default (by installing this package), following *linum-on* or enabling *linum-mode*. One can also use the *linum-relative-toggle* interactive function to switch between relative and non-relative line numbering:
 
     M-x linum-relative-toggle
+
+
+### Backends
+
+
+By default, linum-relative use *linum-mode* as backend, since linum-mode is based on emacs-lisp, you may have performance issue on large file.
+
+Since linum-relative 0.6, if you also use emacs version 26.1 or above, you can setup `linum-relative-backend` to make linum-relative-mode use `display-line-number-mode` as backend, which is implement in C so the performance is really nice. (However some linum-relative's customize function may not work propely)
+Here's how to use `display-line-number-mode` as backend:
+
+```elisp
+     ;; Use `display-line-number-mode` as linum-mode's backend for smooth performance
+	(setq linum-relative-backend 'display-line-numbers-mode)
+```
 
 
 ### Function Documentation
--- linum-relative.el
+++ linum-relative.el
@@ -5,7 +5,7 @@
 ;; Author: coldnew <coldnew.tw@gmail.com>
 ;; Keywords: converience
 ;; X-URL: http://github.com/coldnew/linum-relative
-;; Version: 0.5
+;; Version: 0.6
 
 ;; This file is not part of GNU Emacs.
 
@@ -48,46 +48,76 @@
 ;;
 ;;     (linum-on)
 ;;
-;; and alternatively, by enabling `linum-mode`:
+;; and alternatively, by using command:
 ;;
-;;     M-x linum-mode
+;;     M-x linum-relative-mode
 ;;
 ;; Relative line numbering should already be enabled by default (by installing this package), following *linum-on* or enabling *linum-mode*. One can also use the *linum-relative-toggle* interactive function to switch between relative and non-relative line numbering:
 ;;
 ;;     M-x linum-relative-toggle
 ;;
 
+;;; Backends
+
+;; By default, linum-relative use *linum-mode* as backend, since linum-mode is based on emacs-lisp, you may have performance issue on large file.
+;;
+;; Since linum-relative 0.6, if you also use emacs version 26.1 or above, you can setup `linum-relative-backend' to make linum-relative-mode use `display-line-number-mode' as backend, which is implement in C so the performance is really nice. (However some linum-relative's customize function may not work propely)
+;; Here's how to use `display-line-number-mode' as backend:
+;;
+;; ```elisp
+;;      ;; Use `display-line-number-mode' as linum-mode's backend for smooth performance
+;;	(setq linum-relative-backend 'display-line-numbers-mode)
+;; ```
+
+
 ;;; Code:
 
 (eval-when-compile (require 'cl))
+
+;; display-line-numbers is shipped with emacs 26.0.50
+(when (version<= "26.0.50" emacs-version)
+  (require 'display-line-numbers))
 (require 'linum)
+
 
 (defgroup linum-relative nil
   "Show relative line numbers on fringe."
   :group 'convenience)
 
 ;;;; Faces
+;; FIXME: no use on `display-line-numbers'
 (defface linum-relative-current-face
   '((t :inherit linum :foreground "#CAE682" :background "#444444" :weight bold))
-  "Face for displaying current line."
+  "Face for displaying current line.
+
+This won't take effect if you choose `display-line-numbers' backend."
   :group 'linum-relative)
 
 ;;;; Customize Variables
 
+;; FIXME: no use on `display-line-numbers'
 (defcustom linum-relative-current-symbol "0"
   "The symbol you want to show on the current line, by default it is 0.
    You can use any string like \"->\". If this variable is empty string,
-linum-releative will show the real line number at current line."
+linum-releative will show the real line number at current line.
+
+This won't take effect if you choose `display-line-numbers' backend."
   :type 'string
   :group 'linum-relative)
 
+;; FIXME: no use on `display-line-numbers'
 (defcustom linum-relative-plusp-offset 0
-  "Offset to use for positive relative line numbers."
+  "Offset to use for positive relative line numbers.
+
+This won't take effect if you choose `display-line-numbers' backend."
   :type 'integer
   :group 'linum-relative)
 
+;; FIXME: no use on `display-line-numbers'
 (defcustom linum-relative-format "%3s"
-  "Format for each line. Good for adding spaces/paddings like so: \" %3s \""
+  "Format for each line. Good for adding spaces/paddings like so: \" %3s \"
+
+This won't take effect if you choose `display-line-numbers' backend."
   :type 'string
   :group 'linum-relative)
 
@@ -96,13 +126,26 @@ linum-releative will show the real line number at current line."
   :type 'string
   :group 'linum-relative)
 
+(defcustom linum-relative-backend 'linum-mode
+  "The default backend for `linum-relative', by default we use
+`linum-mode' (slow), you can switch to `display-line-numbers' if
+you has emacs-version greater than 26.0.50."
+  :group 'linum-relative
+  :type '(choice (const :tag "Use display-line-numbers-mode as backend" display-line-numbers-mode)
+                 (other :tag "Use linum-mode as backend" linum-mode)))
+
 ;;;; Internal Variables
 
+;; NOTE: no use on `display-line-numbers'
 (defvar linum-relative-last-pos 0
   "Store last position.")
 
+;; NOTE: no use on `display-line-numbers'
 (defvar linum-relative-user-format linum-format
   "Store the users linum-format")
+
+(defvar linum-relative-user-type (bound-and-true-p display-line-numbers-type)
+  "Store the user's `display-line-number-type' value")
 
 ;;;; helm support
 (defvar helm-buffer)
@@ -122,7 +165,7 @@ linum-releative will show the real line number at current line."
 
 (defun linum-relative-for-helm ()
   (linum-relative-with-helm-buffer
-    (make-local-variable 'linum-relative-last-pos))
+   (make-local-variable 'linum-relative-last-pos))
   (linum-update helm-buffer))
 
 ;;;; Advices
@@ -130,53 +173,76 @@ linum-releative will show the real line number at current line."
   "This advice get the last position of linum."
   (if (linum-relative-in-helm-p)
       (setq linum-relative-last-pos (helm-candidate-number-at-point))
-    (setq linum-relative-last-pos (line-number-at-pos))))
+      (setq linum-relative-last-pos (line-number-at-pos))))
 
 ;;;; Functions
 (defun linum-relative (line-number)
   (when (linum-relative-in-helm-p)
     (linum-relative-with-helm-buffer
-      (if (looking-at helm-candidate-separator)
-          (setq line-number (save-excursion
-                              (forward-line 1) (helm-candidate-number-at-point)))
-        (setq line-number (helm-candidate-number-at-point)))))
+     (if (looking-at helm-candidate-separator)
+         (setq line-number (save-excursion
+                             (forward-line 1) (helm-candidate-number-at-point)))
+         (setq line-number (helm-candidate-number-at-point)))))
   (let* ((diff1 (abs (- line-number linum-relative-last-pos)))
          (diff (if (minusp diff1)
                    diff1
-                 (+ diff1 linum-relative-plusp-offset)))
+                   (+ diff1 linum-relative-plusp-offset)))
          (current-p (= diff linum-relative-plusp-offset))
          (current-symbol (if (and linum-relative-current-symbol current-p)
                              (if (string= "" linum-relative-current-symbol)
                                  (number-to-string line-number)
-                               linum-relative-current-symbol)
-                           (number-to-string diff)))
+                                 linum-relative-current-symbol)
+                             (number-to-string diff)))
          (face (if current-p 'linum-relative-current-face 'linum)))
     (if (and (linum-relative-in-helm-p)
              (linum-relative-with-helm-buffer
-               (or (looking-at helm-candidate-separator)
-                   (eq (point-at-bol) (point-at-eol))
-                   (helm-pos-header-line-p))))
+              (or (looking-at helm-candidate-separator)
+                  (eq (point-at-bol) (point-at-eol))
+                  (helm-pos-header-line-p))))
         (propertize (format linum-relative-format current-symbol) 'invisible t)
-      (propertize (format linum-relative-format current-symbol) 'face face))))
+        (propertize (format linum-relative-format current-symbol) 'face face))))
 
 
 (defun linum-relative-on ()
   "Turn ON linum-relative."
-  (unless (eq linum-format 'linum-relative)
-    (setq linum-relative-user-format linum-format)
-    (setq linum-format 'linum-relative)))
+  (cond
+   ;; if use `display-line-numbers-mode'
+   ((eq linum-relative-backend 'display-line-numbers-mode)
+    (unless (eq linum-relative-user-type 'relative)
+      (setq linum-relative-user-type display-line-numbers-type)
+      (setq display-line-numbers-type 'relative))
+    (display-line-numbers-mode 1))
+   ;; default for linum-mode backend
+   (t (unless (eq linum-format 'linum-relative)
+        (setq linum-relative-user-format linum-format)
+        (setq linum-format 'linum-relative))
+      (linum-mode 1))))
 
 (defun linum-relative-off ()
   "Turn OFF linum-relative."
-  (setq linum-format linum-relative-user-format))
+  (cond
+   ;; if use `display-line-numbers-mode'
+   ((eq linum-relative-backend 'display-line-numbers-mode)
+    (setq display-line-numbers-type linum-relative-user-type)
+    (display-line-numbers-mode -1))
+   ;; default for linum-mode backend
+   (t (setq linum-format linum-relative-user-format)
+      (linum-mode -1))))
 
 ;;;###autoload
 (defun linum-relative-toggle ()
   "Toggle between linum-relative and linum."
   (interactive)
-  (if (eq linum-format 'linum-relative)
-      (linum-relative-off)
-    (linum-relative-on)))
+  (cond
+   ;; if use `display-line-numbers-mode'
+   ((eq linum-relative-backend 'display-line-numbers-mode)
+    (if (eq display-line-numbers-type 'relative)
+        (linum-relative-off)
+        (linum-relative-on)))
+   ;; default for linum-mode backend
+   (t (if (eq linum-format 'linum-relative)
+          (linum-relative-off)
+          (linum-relative-on)))))
 
 ;;;###autoload
 (define-minor-mode linum-relative-mode
@@ -184,16 +250,13 @@ linum-releative will show the real line number at current line."
   :group 'linum-relative
   :lighter linum-relative-lighter
   (if linum-relative-mode
-      (progn
-        (linum-relative-on)
-        (linum-mode 1))
-      (linum-relative-off)
-      (linum-mode -1)))
+      (linum-relative-on)
+      (linum-relative-off)))
 
 ;;;###autoload
 (define-global-minor-mode linum-relative-global-mode
-    linum-relative-mode (lambda () (unless (linum-relative-in-helm-p)
-                                     (linum-relative-mode 1))))
+  linum-relative-mode (lambda () (unless (linum-relative-in-helm-p)
+                              (linum-relative-mode 1))))
 
 ;;;; Interaction of helm with linum-relative
 
@@ -202,7 +265,7 @@ linum-releative will show the real line number at current line."
 
 ;;;###autoload
 (define-minor-mode helm-linum-relative-mode
-    "Turn on `linum-relative-mode' in helm."
+  "Turn on `linum-relative-mode' in helm."
   :group 'helm
   (if helm-linum-relative-mode
       (progn
